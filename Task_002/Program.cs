b601e06fe8cf719a596e8c5911b33a91fef4e767
Daniils_Kackins <daniil.kackin@gmail.com>
2023-07-26 23:10:58 +0100

    Добавил задачу 66

---
+++ Task_002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine($"Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.\nM = 1; N = 15 -> 120\nM = 4; N = 8. -> 30");
+
+Console.Write($"\nВведите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int start, int end)
+{
+    if (start > end) return 0;
+    return start + SumNumbers(start + 1, end);
+}
+
+Console.WriteLine($"Сумма чисел в промежутке от {M} до {N}: {SumNumbers(M,N)}");
